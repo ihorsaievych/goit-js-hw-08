67a5d850e76945c5edcd80cf92e05cdc513afd3f
Ihor Saievych <55617503+ihorsaievych@users.noreply.github.com>
2022-10-11 15:17:16 +0300

    task 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import onLocalStorage from './local';
 
+const iframe = document.querySelector('#vimeo-player');
+const localStorageKey = 'videoplayer-current-time';
+const throtledOnTimeUpdate = throttle(onTimeUpdate, 1000);
+const player = new Player(iframe);
+
+player.on('timeupdate',throtledOnTimeUpdate);
+
+player.setCurrentTime(loadPosition());
+
+function onTimeUpdate(event) {
+  savePosition(event.seconds);
+}
+
+function savePosition(value) {
+  onLocalStorage.save(localStorageKey, String(value));
+  console.log(String(value));
+}
+ 
+function loadPosition() {
+  return onLocalStorage.load(localStorageKey) || 0;
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+import onLocalStorage from './local';
 
+const localStorageKey = 'feedback-form-state';
+const inputRef = document.querySelector('input');
+const textareaRef = document.querySelector('textarea');
+const formRef = document.querySelector('.feedback-form');
+const timeDelay = 500;
+const initialFormData = {
+  email: '',
+  message: '',
+};
+let currentFormData = initialFormData;
+
+fillForm();
+
+formRef.addEventListener('input', throttle(onFormInput, timeDelay));
+formRef.addEventListener('submit', onFeedbackFormSubmit);
+
+function onFormInput(event) {
+  currentFormData = {
+    ...currentFormData,
+    [event.target.name]: event.target.value,
+  };
+  onLocalStorage.save(localStorageKey, currentFormData);
+}
+
+function onFeedbackFormSubmit(event) {
+    event.preventDefault();
+
+  formRef.reset();
+  console.log(currentFormData);
+  currentFormData = initialFormData;
+  onLocalStorage.save(localStorageKey, currentFormData);
+}
+
+function loadFeedbackFormState() {
+  currentFormData =
+    onLocalStorage.load(localStorageKey) || initialFormData;
+  return currentFormData;
+}
+
+function fillForm() {
+  const { email, message } = loadFeedbackFormState();
+  inputRef.value = email;
+  textareaRef.value = message;
+}
